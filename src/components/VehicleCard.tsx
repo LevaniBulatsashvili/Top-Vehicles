bf527e67180dff2c70e272404d3f00132b3d6df8
Levan <levan.bulatsashvili@gmail.com>
2024-07-27 15:53:04 +0400

    ref: improve Vehicle component

---
+++ src/components/VehicleCard.tsx
@@ -0,0 +1,27 @@
+import styles from "./VehicleCard.module.css";
+import VehicleCardProps from "../types/VehicleCardProps";
+import Button from "./UI/Button";
+
+export default function VehicleCard({
+  id,
+  title,
+  loc,
+  price,
+  img,
+}: VehicleCardProps) {
+  return (
+    <article className={styles["vehicle-card"]}>
+      <Button to={`/vehicle/${id}`}>
+        <img src={img} alt={title} />
+      </Button>
+
+      <div className={styles["vehicle-info"]}>
+        <p>{loc}</p>
+        <p>
+          <Button to={`/vehicle/${id}`}>{title}</Button>
+        </p>
+        <h3>{price} $</h3>
+      </div>
+    </article>
+  );
+}
